08df8edb75114910d553f78381995361e782552c
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-16 12:12:01 +0100

    wip

--- posts/post-router.js
+++ posts/post-router.js
@@ -9,7 +9,20 @@ function getAll() {
     .select('id', 'title', 'contents')
     .from('posts')
 }
+function getById(id) {
+  return db
+    
+}
 // database access using knex END
+router.get('/:id', async (req, res) => {
+  try {
+    const data = await getById(req.params.id)
+    res.status(200).json(data)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Fatal error getting post by id' })
+  }
+})
 
 router.get('/', async (req, res) => {
   try {
@@ -20,10 +33,6 @@ router.get('/', async (req, res) => {
     res.status(500).json({ message: 'Fatal error getting all posts' })
   }
 })
-
-router.get('/:id', (req, res) => {
-
-});
 
 router.post('/', (req, res) => {
 
