8c7df130ac7e1498416e83bb8eeadbaa7142876d
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-13 13:33:49 -0400

    Complete GET/GET by ID endpoints

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,11 +6,26 @@ const db = require('../data/db-config.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    db.select('*').from('posts')
+    .then(posts => {
+        res.status(200).json({data: posts})
+    }).catch(error => {
+        console.log(error);
+        res.status(500).json({error: error.message })
+    });
 });
 
 router.get('/:id', (req, res) => {
-
+    db('posts')
+    .where({ id: req.params.id })
+    // .where('id', req.params.id)
+    // .first()
+    .then(post => {
+        res.status(200).json({data: post})
+    .catch(error => {
+        res.status(500).json({error: error.message})
+    })
+    })
 });
 
 router.post('/', (req, res) => {
